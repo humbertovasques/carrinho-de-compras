bcc567e1e035aa6351132c174c61d09fbb59479b
Humberto Vasques <humbertovasques.1b@gmail.com>
2023-06-08 17:04:20 -0300

    Adiciona Testes para o repositório

--- Tests/Unit/Repository/ItemRepositoryTest.cs
+++ Tests/Unit/Repository/ItemRepositoryTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Api.DTOs;
 using Api.Repository;
+using FluentAssertions;
 
 namespace Tests.Unit.Repository
 {
@@ -11,14 +12,42 @@ namespace Tests.Unit.Repository
     {
         ItemRepository sut = new ItemRepository();
 
-        // [Fact]
-        // public void AddItem_AdicionaUmItemNaLista()
-        // {
-        //     // Configuração
-        //     ItemDTO item = new ItemDTO { Nome = "Item 1", Peso = 0.25, Valor = 100 };
+        [Fact]
+        public void AddItem_AdicionaUmItemNaLista()
+        {
+            // Configuração
+            ItemDTO item = new ItemDTO { Nome = "Item 1", Peso = 0.25, Valor = 100 };
 
-        //     // Act
-        //     var result = sut.AddItem(item);
-        // }
+            // Ação
+            sut.AddItem(item);
+
+            // Verificação
+            sut.GetAllItems().Count.Should().Be(1);
+        }
+
+        [Fact]
+        public void GetAllItems_RetornaUmaListadeItems()
+        {
+            // Configuração
+            var itemList = new List<ItemDTO>
+            {   
+                new ItemDTO {Nome = "Item 1", Peso = 0.25, Valor = 100 },
+                new ItemDTO {Nome = "Item 2", Peso = 0.25, Valor = 100 },
+                new ItemDTO {Nome = "Item 3", Peso = 0.25, Valor = 100 },
+            };
+
+            foreach (var item in itemList)
+            {
+                sut.AddItem(item);
+            }
+
+            // Ação
+            var result = sut.GetAllItems();
+
+            // Verificação
+            result.Count.Should().Be(itemList.Count);
+            result.Should().BeOfType<List<ItemDTO>>();
+
+        }
     }
 }
